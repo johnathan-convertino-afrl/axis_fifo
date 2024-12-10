eb7dc4b64109216d16f7177a0ebb825afeff03fe
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-12-10 15:35:23 -0500

    add cocotb test bench

--- tb/tb_axis.v
+++ tb/tb_axis.v
@@ -1,36 +1,53 @@
 //******************************************************************************
-/// @file    tb_axis.v
-/// @author  JAY CONVERTINO
-/// @date    2022.10.24
-/// @brief   Generic AXIS test bench top with verification.
-/// @details This generic top and mod axis test bench is created for a FIFO.
-///          It will have to be altered for other devices that alter the input
-///          for some sort of output.
-///
-/// @LICENSE MIT
-///  Copyright 2022 Jay Convertino
-///
-///  Permission is hereby granted, free of charge, to any person obtaining a copy
-///  of this software and associated documentation files (the "Software"), to 
-///  deal in the Software without restriction, including without limitation the
-///  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-///  sell copies of the Software, and to permit persons to whom the Software is 
-///  furnished to do so, subject to the following conditions:
-///
-///  The above copyright notice and this permission notice shall be included in 
-///  all copies or substantial portions of the Software.
-///
-///  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-///  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-///  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-///  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-///  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-///  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-///  IN THE SOFTWARE.
+// file:    tb_axis.v
+//
+// author:  JAY CONVERTINO
+//
+// date:    2024/12/09
+//
+// about:   Brief
+// Test bench for axis_fifo using axis stim and clock stim.
+//
+// license: License MIT
+// Copyright 2024 Jay Convertino
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
 //******************************************************************************
 
-`timescale 1 ns/10 ps
+`timescale 1ns/100ps
 
+/*
+ * Module: tb_axis
+ *
+ * Test bench for axis_fifo. This will run a file through the system
+ * and write its output. These can then be compared to check for errors.
+ * If the files are identical, no errors. A FST file will be written.
+ *
+ * Parameters:
+ *
+ *   IN_FILE_NAME  - File name for input.
+ *   OUT_FILE_NAME - File name for output.
+ *   FIFO_DEPTH    - Number of transactions to buffer.
+ *   RAND_READY    - 0 = no random ready. 1 = randomize ready.
+ *
+ */
 module tb_axis #(
   parameter IN_FILE_NAME = "in.bin",
   parameter OUT_FILE_NAME = "out.bin",
@@ -79,28 +96,37 @@ module tb_axis #(
     #1;
   end
   
+  //Group: Instantiated Modules
+
+  /*
+   * Module: clk_stim
+   *
+   * Generate a 50/50 duty cycle set of clocks and reset.
+   */
   clk_stimulus #(
-    .CLOCKS(2), // # of clocks
-    .CLOCK_BASE(1000000), // clock time base mhz
-    .CLOCK_INC(10), // clock time diff mhz
-    .RESETS(2), // # of resets
-    .RESET_BASE(2000), // time to stay in reset
-    .RESET_INC(100) // time diff for other resets
+    .CLOCKS(2),
+    .CLOCK_BASE(1000000),
+    .CLOCK_INC(10),
+    .RESETS(2),
+    .RESET_BASE(2000),
+    .RESET_INC(100)
   ) clk_stim (
-    //clk out ... maybe a vector of clks with diff speeds.
     .clkv({tb_dut_clk, tb_stim_clk}),
-    //rstn out ... maybe a vector of rsts with different off times
     .rstnv({tb_dut_rstn, tb_stim_rstn}),
     .rstv()
   );
   
+  /*
+   * Module: slave_axis_stim
+   *
+   * Device under test SLAVE stimulus module.
+   */
   slave_axis_stimulus #(
     .BUS_WIDTH(BUS_WIDTH),
     .USER_WIDTH(USER_WIDTH),
     .DEST_WIDTH(DEST_WIDTH),
     .FILE(IN_FILE_NAME)
   ) slave_axis_stim (
-    // output to slave
     .m_axis_aclk(tb_stim_clk),
     .m_axis_arstn(tb_stim_rstn),
     .m_axis_tvalid(tb_stim_valid),
@@ -113,6 +139,11 @@ module tb_axis #(
     .eof(tb_eof)
   );
   
+  /*
+   * Module: dut
+   *
+   * Device under test, axis_fifo
+   */
   axis_fifo #(
     .FIFO_DEPTH(FIFO_DEPTH),
     .COUNT_WIDTH(8),
@@ -124,7 +155,6 @@ module tb_axis #(
     .COUNT_DELAY(1),
     .COUNT_ENA(1)
   ) dut (
-    // input
     .s_axis_aclk(tb_stim_clk),
     .s_axis_arstn(tb_stim_rstn),
     .s_axis_tvalid(tb_stim_valid),
@@ -134,7 +164,6 @@ module tb_axis #(
     .s_axis_tlast(tb_stim_last),
     .s_axis_tuser(tb_stim_user),
     .s_axis_tdest(tb_stim_dest),
-    // output
     .m_axis_aclk(tb_dut_clk),
     .m_axis_arstn(tb_dut_rstn),
     .m_axis_tvalid(tb_dut_valid),
@@ -144,12 +173,16 @@ module tb_axis #(
     .m_axis_tlast(tb_dut_last),
     .m_axis_tuser(tb_dut_user),
     .m_axis_tdest(tb_dut_dest),
-    // data count
     .data_count_aclk(tb_cnt_clk),
     .data_count_arstn(tb_cnt_rstn),
     .data_count(tb_cnt_data)
   );
   
+  /*
+   * Module: master_axis_stim
+   *
+   * Devie under test MASTER stimulus module.
+   */
   master_axis_stimulus #(
     .BUS_WIDTH(BUS_WIDTH),
     .USER_WIDTH(USER_WIDTH),
@@ -157,7 +190,6 @@ module tb_axis #(
     .RAND_READY(RAND_READY),
     .FILE(OUT_FILE_NAME)
   ) master_axis_stim (
-    // write
     .s_axis_aclk(tb_dut_clk),
     .s_axis_arstn(tb_dut_rstn),
     .s_axis_tvalid(tb_dut_valid),
